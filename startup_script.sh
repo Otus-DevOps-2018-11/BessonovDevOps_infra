15bda3c4f7f24b3f36d2ee9f8068274d066884e7
Daniil <bessonov.devops@gmail.com>
2019-03-02 19:21:22 +0300

    some changes in README.md

--- startup_script.sh
+++ startup_script.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-# use sudo to run script
-apt-get update && apt-get upgrade -y && \
+apt-get update && apt-get upgrade -y
 apt-get install -y ruby-full ruby-bundler build-essential
-gem install bundler
 
-# use sudo to run script
 apt-get-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-
-mongo_apt="/etc/apt-get/sources.list.d/mongodb-org-3.2.list"
-
-if [ ! -f $mongo_apt ]
-then
-	echo "deb http://repo.mongodb.org/apt-get/ubuntu $(lsb_release -cs)/mongodb-org/3.2 multiverse" \
-	> $mongo_apt
-fi
-apt-get update
-apt-get install -y mongodb-org && \
-systemctl enable mongod && \
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get install -y mongodb-org
 systemctl start mongod
+systemctl enable mongod
 
-cd ~
-git clone -b monolith https://github.com/express42/reddit.git && \
-cd reddit && bundle install && \
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
 puma -d
 
