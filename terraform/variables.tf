56ea6008d1e8ba1b4b38928eff5ee2e61e1d77f4
Daniil <bessonov.devops@gmail.com>
2019-03-24 18:16:44 +0300

    all instance separated by modules

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,6 +15,16 @@ variable private_key_path {
   description = "Path to the private key used for ssh access"
 }
 
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
+}
+
 variable zone {
   description = "GCP zone for deploy app"
   default     = "europe-west1-b"
