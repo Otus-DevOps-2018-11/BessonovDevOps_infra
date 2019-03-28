dabe7526424ebca56069909cb46377d57270b5cc
Daniil <bessonov.devops@gmail.com>
2019-03-28 22:31:13 +0300

    refactored prod

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -7,9 +7,10 @@ provider "google" {
 module "app" {
   source           = "../modules/app"
   public_key_path  = "${var.public_key_path}"
-  private_key_path = "${var.private_key_path}"
   zone             = "${var.zone}"
   app_disk_image   = "${var.app_disk_image}"
+  private_key_path = "${var.private_key_path}"
+  db_internal_ip   = "${module.db.db_internal_ip}"
 }
 
 module "db" {
